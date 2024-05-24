14e12a53e0068545cecc7a8388ad4c93ea75dda4
Esther Aragoneses Hernández <estheraraher@gmail.com>
2024-05-24 09:26:46 +0200

    Declarar metodos y atributos peon

--- Peon.cpp
+++ Peon.cpp
@@ -1,1 +1,17 @@
 #pragma once
+#include "Peon.h"
+
+Peon::Peon(void)
+{
+
+}
+
+Peon::~Peon(void)
+{
+
+}
+
+bool Peon::mover(Jugada &)
+{
+    
+}
--- Peon.h
+++ Peon.h
@@ -1,1 +1,11 @@
 #pragma once
+#include "Pieza.h"
+
+class Peon : public Pieza
+{
+public:
+	Peon(void);
+	virtual ~Peon(void);
+
+	bool mover (struct Jugada&);
+};
