cd18eaf96bcd16ececd6b02a026244b1c38c0c59
Esther Aragoneses Hernández <estheraraher@gmail.com>
2024-05-24 09:17:02 +0200

    Declarar atributos y metodos torre

--- Torre.cpp
+++ Torre.cpp
@@ -1,1 +1,18 @@
 #pragma once
+
+#include "Torre.h"
+
+Torre::Torre(void)
+{
+
+}
+
+Torre::~Torre(void)
+{
+
+}
+
+bool Torre::mover(Jugada &)
+{
+    
+}
--- Torre.h
+++ Torre.h
@@ -1,1 +1,11 @@
 #pragma once
+#include "Pieza.h"
+
+class Torre : public Pieza
+{
+public:
+	Torre(void);
+	virtual ~Torre(void);
+
+	bool mover (struct Jugada&);
+};
