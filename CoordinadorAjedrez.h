a2886a6f58992fa86658f48eeacbd5da2fb835cc
cesargonzalez225 <159026529+cesargonzalez225@users.noreply.github.com>
2024-05-25 17:55:18 +0200

    Update CoordinadorAjedrez.h

--- CoordinadorAjedrez.h
+++ CoordinadorAjedrez.h
@@ -1,16 +1,20 @@
 #pragma once
 #include "Mundo.h"
 #include "ETSIDI.h"
+#include "Tablero.h"
 class CoordinadorAjedrez
 {
 public:
 	CoordinadorAjedrez();
+	int leerCasilla(Casilla& casilla);
 	virtual ~CoordinadorAjedrez();
 	void tecla(unsigned char key);
 	void mueve();
 	void dibuja();
+	void raton(Casilla&);
 protected:
 	Mundo mundo;
 	enum Estado { INICIO, JUEGO, FIN, GAMEOVER, PAUSA };
 	Estado estado;
+	Casilla origen, destino;
 };
