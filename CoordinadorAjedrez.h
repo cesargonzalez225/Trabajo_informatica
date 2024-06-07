21e26dcdb45ccff68a6faf1bf1aee3e13a28aca6
DavidCabGon <163419943+DavidCabGon@users.noreply.github.com>
2024-06-07 19:38:13 +0200

    Update CoordinadorAjedrez.h

--- CoordinadorAjedrez.h
+++ CoordinadorAjedrez.h
@@ -2,6 +2,7 @@
 #include "Mundo.h"
 #include "ETSIDI.h"
 #include "Tablero.h"
+#include "Pieza.h"
 class CoordinadorAjedrez
 {
 public:
@@ -12,9 +13,14 @@ public:
 	void mueve();
 	void dibuja();
 	void raton(Casilla&);
+	Tablero tablero;
+	enum Turno {BLANCAS, NEGRAS};
+	Turno turno;
+	int accion; // 0:Ninguna pieza seleccionada  1:Una pieza seleccionada 2:Una pieza y un destino seleccionado
 protected:
 	Mundo mundo;
 	enum Estado { INICIO, JUEGO, FIN, GAMEOVER, PAUSA };
 	Estado estado;
 	Casilla origen, destino;
+	Pieza pieza;
 };
