023be921b5cbb1162044a417b9fbe2440a0d589b
Esther Aragoneses Hernández <163413466+estheraragoneses@users.noreply.github.com>
2024-06-07 20:54:58 +0200

    Dibujar piezas
    Se dibujan las piezas empleando un switch case, con la imagen correspondiente para cada número, tal y como se define la matriz en tablero.cpp. Por ejemplo, al detectar el número 1, se imprimirá la imagen relativa al peón.

--- Mundo.cpp
+++ Mundo.cpp
@@ -12,4 +12,66 @@ Mundo::~Mundo()
 void Mundo::dibuja()
 {
     tablero.dibuja();
+    dibujarPiezas(tablero);
+}
+
+void Mundo::dibujarPiezas(Tablero& tablero)
+{
+	int pieza;
+
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			DibujarPiezas dibujarPiezas;
+	
+			int m = i;
+			int n = j;
+			pieza = tablero.tablero[n][m];
+
+			switch (pieza)
+			{
+			case -1:
+				dibujarPiezas.dibujaPeonBlanca(m, n);
+				break;
+			case -3:
+				dibujarPiezas.dibujaCaballoBlanca(m, n);
+				break;
+			case -4:
+				dibujarPiezas.dibujaAlfilBlanca(m, n);
+				break;
+			case -5:
+				dibujarPiezas.dibujaTorreBlanca(m, n);
+				break;
+			case -7:
+				dibujarPiezas.dibujaReinaBlanca(m, n);
+				break;
+			case -8:
+				dibujarPiezas.dibujaReyBlanca(m, n);
+				break;
+
+			case 1:
+				dibujarPiezas.dibujaPeonNegra(m, n);
+				break;
+			case 3:
+				dibujarPiezas.dibujaCaballoNegra(m, n);
+				break;
+			case 4:
+				dibujarPiezas.dibujaAlfilNegra(m, n);
+				break;
+			case 5:
+				dibujarPiezas.dibujaTorreNegra(m, n);
+				break;
+			case 7:
+				dibujarPiezas.dibujaReinaNegra(m, n);
+				break;
+			case 8:
+				dibujarPiezas.dibujaReyNegra(m, n);
+				break;
+                
+			case 0:
+				break;
+			}
+		}
+	}
 }
