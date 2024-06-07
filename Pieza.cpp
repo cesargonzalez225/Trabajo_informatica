f68a9462bf72b1fd2f5d316b5d4bd0cf45fa6052
CarlosAlonso11 <163113964+CarlosAlonso11@users.noreply.github.com>
2024-06-07 11:11:33 +0200

    Update Pieza.cpp

--- Pieza.cpp
+++ Pieza.cpp
@@ -201,73 +201,73 @@ void Pieza::vermovimiento(Tablero& tablero, Casilla origen, int turno)
  }
  void Pieza::movimientoTorre(Tablero tablero, Casilla origen)
  {
-	 int x2 = 1, y2 = 1;
-	 int x1 = origen.x;
-	 int y1 = origen.y;
-	 int c = 0, s = 0, i = 1;
+	int x2 = 1, y2 = 1;
+int x1 = origen.x;
+int y1 = origen.y;
+int c = 0, s = 0, i = 1;
 
-	 for (x2 = 1; x2 < 9; x2++)
+for (x2 = 1; x2 < 9; x2++)
+{
+ for (y2 = 1; y2 < 9; y2++)
+ {
+	 if ((x2 - x1) != 0 && (y2 - y1) != 0) {//Comprueba que tenga movimiento recto
+		 c = 1;
+	 }
+	 else if ((tablero.tablero[y1-1][x1-1] < 0 && tablero.tablero[y2-1][x2-1] < 0) || (tablero.tablero[y1-1][x1-1] > 0 && tablero.tablero[y2-1][x2-1] > 0)) { //Mira si es una pieza enemiga o vacia
+		 c = 1;
+	 }
+	 else if ((x2 - x1) > 0 && (y2 - y1) == 0)// Caso 1 Comprobacion derecha
 	 {
-		 for (y2 = 1; y2 < 9; y2++)
+		 while (c == 0 && x1 + i != x2)//Repetir bucle hasta que haya una ficha en medio o que llegue a p2
 		 {
-			 if ((x2 - x1) != 0 && (y2 - y1) != 0) {//Comprueba que tenga movimiento recto
+			 if (tablero.tablero[y1-1][x1] != 0)//Si hay una ficha c=1
+			 {
 				 c = 1;
 			 }
-			 else if ((tablero.tablero[x1][y1] < 0 && tablero.tablero[x2][y2] < 0) || (tablero.tablero[x1][y1] > 0 && tablero.tablero[x2][y2] > 0)) { //Mira si es una pieza enemiga o vacia
-				 c = 1;
-			 }
-			 else if ((x2 - x1) > 0 && (y2 - y1) == 0)// Caso 1 Comprobacion derecha
-			 {
-				 while (c == 0 && x1 + i != x2)//Repetir bucle hasta que haya una ficha en medio o que llegue a p2
-				 {
-					 if (tablero.tablero[x1 + i][y1] != 0)//Si hay una ficha c=1
-					 {
-						 c = 1;
-					 }
-					 i++;
-				 }
-			 }
-			 else if ((x2 - x1) < 0 && (y2 - y1) == 0)// Caso 2 Comprobacion izquierda
-			 {
-				 while (c == 0 && x1 - i != x2)//Repetir bucle hasta que haya una ficha en medio o que llegue a p2
-				 {
-					 if (tablero.tablero[x1 - i][y1] != 0)//Si hay una ficha c=1
-					 {
-						 c = 1;
-					 }
-					 i++;
-				 }
-			 }
-			 else if ((x2 - x1) == 0 && (y2 - y1) < 0)// Caso 3 Comprobacion arriba
-			 {
-				 while (c == 0 && y1 - i != y2)//Repetir bucle hasta que haya una ficha en medio o que llegue a p2
-				 {
-					 if (tablero.tablero[x1][y1 - i] != 0)//Si hay una ficha c=1
-					 {
-						 c = 1;
-					 }
-					 i++;
-				 }
-			 }
-			 else if ((x2 - x1) == 0 && (y2 - y1) > 0)// Caso 4 Comprobacion abajo
-			 {
-				 while (c == 0 && y1 + i != y2)//Repetir bucle hasta que haya una ficha en medio o que llegue a p2
-				 {
-					 if (tablero.tablero[x1][y1 + i] != 0)//Si hay una ficha c=1
-					 {
-						 c = 1;
-					 }
-					 i++;
-				 }
-			 }
-			 if (c == 0) {
-				 matriz.tablero[x2][y2] = 1;
-			 }
-			 else {
-				 matriz.tablero[x2][y2] = 0;
-			 }
+			 i++;
 		 }
 	 }
+	 else if ((x2 - x1) < 0 && (y2 - y1) == 0)// Caso 2 Comprobacion izquierda
+	 {
+		 while (c == 0 && x1 - i != x2)//Repetir bucle hasta que haya una ficha en medio o que llegue a p2
+		 {
+			 if (tablero.tablero[y1-1][x1-2] != 0)//Si hay una ficha c=1
+			 {
+				 c = 1;
+			 }
+			 i++;
+		 }
+	 }
+	 else if ((x2 - x1) == 0 && (y2 - y1) < 0)// Caso 3 Comprobacion arriba
+	 {
+		 while (c == 0 && y1 - i != y2)//Repetir bucle hasta que haya una ficha en medio o que llegue a p2
+		 {
+			 if (tablero.tablero[y1-2][x1-1] != 0)//Si hay una ficha c=1
+			 {
+				 c = 1;
+			 }
+			 i++;
+		 }
+	 }
+	 else if ((x2 - x1) == 0 && (y2 - y1) > 0)// Caso 4 Comprobacion abajo
+	 {
+		 while (c == 0 && y1 + i != y2)//Repetir bucle hasta que haya una ficha en medio o que llegue a p2
+		 {
+			 if (tablero.tablero[y1][x1-1] != 0)//Si hay una ficha c=1
+			 {
+				 c = 1;
+			 }
+			 i++;
+		 }
+	 }
+	 if (c == 0) {
+		 matriz.tablero[y2-1][x2-1] = 1;
+	 }
+	 else {
+		 matriz.tablero[y2-1][x2-1] = 0;
+	 }
+ }
+}
  }
  void Pieza::movimientoAlfil(Tablero tablero, Casilla origen)
  {
