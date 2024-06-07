c030b7714b979e1375f7e763de8c19042d66ea14
cesargonzalez225 <159026529+cesargonzalez225@users.noreply.github.com>
2024-06-07 21:26:40 +0200

    Update DibujarPiezas.h
    se actualiza el .h

--- DibujarPiezas.h
+++ DibujarPiezas.h
@@ -1,28 +1,56 @@
-#pragma once
-#include "Casilla.h"
-#include "freeglut.h"
-#include "ETSIDI.h"
+#include "DibujarPiezas.h"
+#include <math.h>
 
-class DibujarPiezas
+DibujarPiezas::DibujarPiezas(void)
 {
+}
 
-public:
-	DibujarPiezas(void);
-	~DibujarPiezas(void);
+DibujarPiezas::~DibujarPiezas(void)
+{
+}
+
+void DibujarPiezas::dibujaPieza(int x, int y, const char* tex)
+{
+    glEnable(GL_TEXTURE_2D);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    GLuint textureId = ETSIDI::getTexture(tex).id;
+    glBindTexture(GL_TEXTURE_2D, textureId);
 
 
-	void dibujaPeonNegra(int x, int y);
-	void dibujaPeonBlanca(int x, int y);
-	void dibujaAlfilNegra(int x, int y);
-	void dibujaAlfilBlanca(int x, int y);
-	void dibujaTorreNegra(int x, int y);
-	void dibujaTorreBlanca(int x, int y);
-	void dibujaCaballoNegra(int x, int y);
-	void dibujaCaballoBlanca(int x, int y);
-	void dibujaReyNegra(int x, int y);
-	void dibujaReyBlanca(int x, int y);
-	void dibujaReinaNegra(int x, int y);
-	void dibujaReinaBlanca(int x, int y);
-	void dibujaPieza(int x, int y, const char* tex);
-};
 
+    glDisable(GL_LIGHTING);
+    glColor3f(1, 1, 1);
+    glPushMatrix();
+
+    
+    glTranslatef(x - 3.5f, 0.3f, y - 5.6f); 
+
+    glRotatef(-90, 1.0f, 0.0f, 0.0f);
+    glRotatef(180, 0.0f, 1.0f, 0.0f);
+
+    
+    glBegin(GL_QUADS);
+   
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex3f(-0.4f, -0.4f, 0.001f); 
+
+   
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex3f(0.4f, -0.4f, 0.001f); 
+
+    
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex3f(0.4f, 0.4f, 0.001f); 
+
+   
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(-0.4f, 0.4f, 0.001f); 
+    glEnd();
+
+    glPopMatrix();
+    glDisable(GL_TEXTURE_2D);
+    glDisable(GL_BLEND);
+    glEnable(GL_LIGHTING);
+}
