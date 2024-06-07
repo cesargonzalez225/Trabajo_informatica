bb6261a18d64ea421964b2ffc73a021a5db4a818
Esther Aragoneses Hernández <163413466+estheraragoneses@users.noreply.github.com>
2024-06-07 21:14:56 +0200

    Funciones de dibujar todas las piezas segun el tipo
    Se declaran las funciones de dibujar segun el tipo. Se inicilizaran en el cpp

--- DibujarPiezas.h
+++ DibujarPiezas.h
@@ -1,1 +1,28 @@
+#pragma once
+#include "Casilla.h"
+#include "freeglut.h"
+#include "ETSIDI.h"
+
+class DibujarPiezas
+{
+
+public:
+	DibujarPiezas(void);
+	~DibujarPiezas(void);
+
+
+	void dibujaPeonNegra(int x, int y);
+	void dibujaPeonBlanca(int x, int y);
+	void dibujaAlfilNegra(int x, int y);
+	void dibujaAlfilBlanca(int x, int y);
+	void dibujaTorreNegra(int x, int y);
+	void dibujaTorreBlanca(int x, int y);
+	void dibujaCaballoNegra(int x, int y);
+	void dibujaCaballoBlanca(int x, int y);
+	void dibujaReyNegra(int x, int y);
+	void dibujaReyBlanca(int x, int y);
+	void dibujaReinaNegra(int x, int y);
+	void dibujaReinaBlanca(int x, int y);
+	void dibujaPieza(int x, int y, const char* tex);
+};
 
